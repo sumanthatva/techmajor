d1d4cc0dcb0420a6bdfe6fafdc4591a3d40da76a
Sumanth <sumanth.van@gmail.com>
2022-06-21 13:03:54 +0530

    Added the first custom component - ProductItem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ProductItem from './components/ProductItem';
 
 function App() {
   return (
     <div className="App">
       <h2> Let's start, Tech Major! </h2>
+      <ProductItem></ProductItem>
     </div>
   );
 }
---
+++ src/components/ProductItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ProductItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> Product Item 1 </p>
+        <p> ₹250 </p>
+      </div>
+    );
+  }
+}
+
+export default ProductItem;

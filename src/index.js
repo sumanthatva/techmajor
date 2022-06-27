f25c75e1043896d6ae6e1c67128cbe627993bb05
Sumanth <sumanth.van@gmail.com>
2022-06-27 22:29:37 +0530

    Routing basics documentation

--- src/index.js
+++ src/index.js
@@ -22,3 +22,12 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
+
+/** Routing basics 
+ * ref: https://reactrouter.com/docs/en/v6/getting-started/tutorial
+ * 1. Use react-router-dom version 6. (https://www.npmjs.com/package/react-router-dom)
+ * 2. BrowserRouter - BrowserRouter is a router implementation that uses the HTML5 history API to keep your UI in sync with the URL
+ * 3. Routes - select the path with the best match for the URL. In version 5, the <Switch> component used to select the first match
+ * 4. Route - A conditionally rendered component. It is rendered if teh path matches the current location (URL)
+ * 5. Link - is an element that lets the user navigate to another page by clicking or tapping on it. 
+*/

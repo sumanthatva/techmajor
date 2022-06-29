a33aaa9530162b7ed2da0ba9e7094c7d82e1ca07
Sumanth <sumanth.van@gmail.com>
2022-06-29 08:55:09 +0530

    reading route params in ProductDetail

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function ProductDetail() {
+  const params = useParams();
+  console.log(params.productId);
   return (
     <div>
       <h2>Product Detail Page</h2>
+      <p>{params.productId}</p>
     </div>
   )
 }

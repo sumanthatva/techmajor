aec9e4497025ed4e1b513d926b390de870f7e434
Sumanth <sumanth.van@gmail.com>
2022-07-01 12:07:32 +0530

    documentation

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetail() {
+  // <Summary/>
+  // routes-use-params
+  // useParams hook gives access to the url params
   const params = useParams();
   console.log(params.productId);
   return (
